f96126989aaf4bd59fd566113353ef1704f90558
Sheikh Shayan Ahmad <64434119+sheikhshayanahmad@users.noreply.github.com>
2022-01-30 19:33:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+var list =document.getElementById("list");
+
+function addTodo(){
+    var todo_item = document.getElementById("todo-item")
+
+    var li = document.createElement('li')
+    var liText = document.createTextNode(todo_item.value)
+    li.appendChild(liText)
+    
+    // create delete button
+    var delBtn = document.createElement("button")
+    var delText =document.createTextNode("DELETE")
+    delBtn.setAttribute("class","btn")
+    delBtn.setAttribute("onclick","deleteItem(this)")
+    delBtn.appendChild(delText)
+
+        // create edit button
+        var editBtn =  document.createElement("button")
+        var editText = document.createTextNode("Edit")
+        editBtn.setAttribute("class","btn")
+        editBtn.setAttribute("onclick","editItem(this)")
+        editBtn.appendChild(editText)
+
+
+    li.appendChild(delBtn)
+    li.appendChild(editBtn)
+
+
+    list.appendChild(li)
+    todo_item.value =""
+    console.log(li)
+
+}
+function deleteItem(f){
+    f.parentNode.remove()
+    console.log(f.parentNode)
+}
+function editItem(e){
+   var val = prompt("enter edit value" ,e.parentNode.firstChild.nodeValue);
+   e.parentNode.firstChild.nodeValue = val;
+}
+
+function deleteAll(){
+    list.innerHTML=""
+}
